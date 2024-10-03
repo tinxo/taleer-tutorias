554d8c55ee9c51fa01d090360c5488b565f7b40f
Martin Rey <m.rey00@gmail.com>
2024-10-03 19:09:30 -0300

    Se agrega un boton para un hola mundo

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace taller
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola mundo");
+        }
     }
 }
